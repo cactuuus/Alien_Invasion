be15d28705e0630e5ddcb766b06e0bf9774dc0a9
cactus <jacopo.bob.calvi@gmail.com>
2023-06-18 15:23:54 +0100

    Added settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+"""The game's settings."""
+
+class Settings():
+    """A class used to handle all settings for the game."""
+
+    def __init__(self):
+        """Initialises the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
